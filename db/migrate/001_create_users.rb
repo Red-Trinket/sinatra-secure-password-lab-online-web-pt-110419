dee0efe7ab4aa200d3401bb3ae023d0614a1470f
Matthew Jones <matts3371@gmail.com>
2020-02-28 02:22:38 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,1 +1,8 @@
-class Creat
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change 
+    create_table :users |t| do 
+      t.string :username
+      t.string :password 
+    end 
+  end 
+end 
